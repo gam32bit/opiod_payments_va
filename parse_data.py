fc822759a642684a419892fd63bd5f8a384ffb48
gam32bit <joseph.caterine@gmail.com>
2023-08-28 17:09:39 -0400

    finished dataset

--- parse_data.py
+++ parse_data.py
@@ -2,6 +2,7 @@ import pdfplumber
 import pandas as pd
 from geopy.geocoders import Nominatim
 
+pd.set_option('display.float_format', '{:.2f}'.format)
 pdf_paths = [
     "files/va-payment-allocations-distributors-year-1.pdf",
     "files/va-payment-allocations-distributors-year-2.pdf",
@@ -45,9 +46,17 @@ def get_data_frame(data):
                     df.loc[clean_county_name] = [None] * (len(columns) - 1)
                     if clean_county_name.split(' ', 1)[0] in fips_df["GU Name"].values:
                         df.loc[clean_county_name, "FIPS"] = fips_df.loc[fips_df["GU Name"] == clean_county_name.split(' ', 1)[0], "County FIPS Code"].iloc[0]
-                df.loc[clean_county_name, columns[counter + 2]] = row[-1]
+                df.loc[clean_county_name, columns[counter + 2]] = round(float(row[-1].replace('$', '').replace(',', '')), 2)
+
+
         counter += 1
-    return df
+    columns_to_sum = ["Distributors_2021", "Distributors_2022", "Distributors_2023", "Janssen_2022", "Janssen_2023"]
+    df["Total_Payment"] = df[columns_to_sum].sum(axis=1)
+    total_total_payment = df["Total_Payment"].sum()
+    df["Percent_of_Total"] = df["Total_Payment"] / total_total_payment
+    final_columns = ["FIPS", "Percent_of_Total"]
+    final_df = df[final_columns]
+    return final_df
     
 my_df = get_data_frame(all_data)
 my_df.to_csv("files/va_data.csv")
